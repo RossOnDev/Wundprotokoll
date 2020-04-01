d79b8975679407d8938093d090d4c33cb3eef1eb
morit <moritz.l.v.p@gmail.com>
2020-04-01 20:31:58 +0200

    script einbinden

---
+++ GUI/main.js
@@ -0,0 +1,8 @@
+function savedata() {
+var size = document.getElementById("size").value
+var name = document.getElementById("name").value
+
+    //python
+eel.savedata(size, name)
+
+}
---
+++ index_01.py
@@ -0,0 +1,9 @@
+import eel
+
+eel.init('GUI')
+
+eel.expose
+def savedata(size, name):
+    list.append(size, name)
+
+eel.start('index.html')
